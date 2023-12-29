4feecadddc2f79212a482af9754d75dd812f2b72
kangwongu <kwg0527@naver.com>
2023-12-29 17:41:38 +0900

    Initial commit

---
+++ jpa_use_shop/src/test/java/com/jpauseshop/JpaUseShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpauseshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUseShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
